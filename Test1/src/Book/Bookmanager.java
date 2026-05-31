372daa4120396a061c1f1516bb46c65e64a615fa
nkw <nkw@DESKTOP-QNJQJBA>
2026-05-31 23:59:43 +0900

    bookmanager 클래스, 테스트, 젠킨스파일 추가

--- Test1/src/Book/Bookmanager.java
+++ Test1/src/Book/Bookmanager.java
@@ -1,5 +1,23 @@
 package Book;
 
-public class Bookmanager {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Bookmanager {
+    private Set<String> books = new HashSet<>();
+
+    public void addBook(String name) {
+        if (books.contains(name)) {
+            throw new IllegalArgumentException("이미 존재하는 책입니다: " + name);
+        }
+        books.add(name);
+    }
+    
+
+    //remove book 구현해주세요
+    
+
+    public boolean hasBook(String name) {
+        return books.contains(name);
+    }
 }
